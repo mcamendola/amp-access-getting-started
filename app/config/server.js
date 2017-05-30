b54118d959bc43e30e1c06458187c81caa1b49f7
Felipe Mello <fmellocosta@gmail.com>
2017-05-30 15:26:31 -0300

    Alterando readme para incluir passos para configurar no Windows; Aprendendo a usar o EJS;

--- app/config/server.js
+++ app/config/server.js
@@ -2,8 +2,10 @@
 
 var express = require('express');
 var app = express();
-
 var SERVER_PORT = 3000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs')
 
 app.listen(SERVER_PORT, function() {
     console.log('Server ON, listening port ' + SERVER_PORT);
